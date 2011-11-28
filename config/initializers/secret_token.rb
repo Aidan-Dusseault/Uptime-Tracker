8c3f5661825788d4639e07b6aeb760fdd9eaa084
unknown <Aidan@.(none)>
2011-11-28 12:28:26 -0500

    Created Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UptimeTracker::Application.config.secret_token = 'baa10a41a318f416fcbf539a42a41d91f0da2c9dd01eb25a8a51baed4f8b907e94820edc4915ef6e74edc8d00d6d0f575664bb41114ee3235446369df06a5b9e'
