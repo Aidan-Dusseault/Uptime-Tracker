ee2fe58bda62d4b102707d16c8fb3130634557ce
Aidan Dusseault <aidandusseault@gmail.com>
2012-01-16 11:45:10 -0500

    This is jon, pushing the stuff up

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,15 +6,4 @@ describe Event do
     @domain = Factory(:domain)
     @attr = { :status_change => 0, :domain => @domain }
   end
-
-  describe "feed" do
-
-    it "should have a feed" do
-      @event.should respond_to(:feed)
-    end
-    it "should include the user's events" do
-    end
-    it "should not include other user's events" do
-    end
-  end
 end
